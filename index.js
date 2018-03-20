e33289cb06b84e29e9301060ced5df3cd26d3a34
Vernon Mensah <vernonmensah@gmail.com>
2018-03-20 09:43:14 -0400

    added graphql and it works!

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
 
 const { PORT } = require('./config');
 const { dbConnect } = require('./db-mongoose');
@@ -12,6 +15,42 @@ app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
 }));
 
 app.use(cors());
+/*  */
+
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books },
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use('/', graphiqlExpress({ endpointURL: '/graphql' }));
 
 function runServer(port = PORT) {
   const server = app
